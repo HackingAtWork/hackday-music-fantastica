74a61e699be17abdb69d302cbb558b759a10e3da
Open Connect <openconnect@us.ibm.com>
2014-01-02 11:56:39 -0500

    Adding random file name serving action

--- Fantastica/Controllers/HomeController.fs
+++ Fantastica/Controllers/HomeController.fs
@@ -6,8 +6,15 @@ open System.Linq
 open System.Web
 open System.Web.Mvc
 open System.Web.Mvc.Ajax
+open System.IO
 
 type HomeController() =
     inherit Controller()
     member this.Index () = this.View()
+
+    member this.NextSong() = 
+        let allFiles = Directory.GetFiles(@"C:\Development\hackday-music-fantastica\Fantastica\Content\mp3s")
+                        |> Array.map (fun x -> Path.GetFileName x)
+        let rnd = new Random()
+        allFiles.[rnd.Next(0,allFiles.Count()-1)]
 

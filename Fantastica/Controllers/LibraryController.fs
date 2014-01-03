e673d773e9efe6be91cfe8f727382d7e3da13bfe
Open Connect <openconnect@us.ibm.com>
2014-01-03 15:00:59 -0500

    fixing the get method

--- Fantastica/Controllers/LibraryController.fs
+++ Fantastica/Controllers/LibraryController.fs
@@ -18,28 +18,27 @@ type LibraryController() =
     let mp3s = DataStore.Instance.SongRepository.FindAll()
     
     member x.Get([<FromUri>]filter:LibraryFilter) =
-        filter
-//      let filterTitle songs = match filter with
-//                                            | h when not (String.IsNullOrWhiteSpace(h.Title)) 
-//                                                -> songs |> List.filter (fun (mp3:Song) -> mp3.Title.ToLower().Contains(h.Title.ToLower()))
-//                                            | _ -> songs
-//
-//      let filterAlbum songs = match filter with
-//                                            | h when not (String.IsNullOrWhiteSpace(h.Album)) 
-//                                                -> songs |> List.filter (fun (mp3:Song) -> mp3.Album.ToLower().Contains(h.Album.ToLower()))
-//                                            | _ -> songs
-//     
-//      let filterArtist songs = match filter with
-//                                            | h when not (String.IsNullOrWhiteSpace(h.Artist)) 
-//                                                -> songs |> List.filter (fun (mp3:Song) -> mp3.Artist.ToLower().Contains(h.Artist.ToLower()))
-//                                            | _ -> songs
-//
-//      let filterAlbumArtist songs = match filter with
-//                                            | h when not (String.IsNullOrWhiteSpace(h.AlbumArtist)) 
-//                                                -> songs |> List.filter (fun (mp3:Song) -> mp3.AlbumArtist.ToLower().Contains(h.AlbumArtist.ToLower()))
-//                                            | _ -> songs
-//
-//      mp3s.ToList() |> filterTitle |> filterAlbum |> filterArtist |> filterAlbumArtist
+      let filterTitle songs = match filter with
+                                | h when not (String.IsNullOrWhiteSpace(h.Title)) 
+                                    -> songs |> List.filter (fun (mp3:Song) -> mp3.Title.ToLower().Contains(h.Title.ToLower()))
+                                | _ -> songs
+
+      let filterAlbum songs = match filter with
+                                | h when not (String.IsNullOrWhiteSpace(h.Album)) 
+                                    -> songs |> List.filter (fun (mp3:Song) -> mp3.Album.ToLower().Contains(h.Album.ToLower()))
+                                | _ -> songs
+     
+      let filterArtist songs = match filter with
+                                | h when not (String.IsNullOrWhiteSpace(h.Artist)) 
+                                    -> songs |> List.filter (fun (mp3:Song) -> mp3.Artist.ToLower().Contains(h.Artist.ToLower()))
+                                | _ -> songs
+
+      let filterAlbumArtist songs = match filter with
+                                    | h when not (String.IsNullOrWhiteSpace(h.AlbumArtist)) 
+                                        -> songs |> List.filter (fun (mp3:Song) -> mp3.AlbumArtist.ToLower().Contains(h.AlbumArtist.ToLower()))
+                                    | _ -> songs
+
+      mp3s.ToList() |> Seq.toList |> filterTitle |> filterAlbum |> filterArtist |> filterAlbumArtist
 
     member x.Put([<FromBody>]songIds:string array)=
         DataStore.Instance.SongRepository.Find(fun (s:Song) -> songIds.Contains(s.Id))

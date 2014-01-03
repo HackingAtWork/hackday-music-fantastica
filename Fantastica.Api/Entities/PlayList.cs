0bd6cd6eaf5ba3c7da0d8dc54c08b2f1eced0133
gc <collateralwebteam@us.ibm.com>
2014-01-03 16:20:36 -0500

    added move next song

--- Fantastica.Api/Entities/PlayList.cs
+++ Fantastica.Api/Entities/PlayList.cs
@@ -1,13 +1,22 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Fantastica.Api.Entities {
   public class PlayList {
     public IEnumerable<string> SongIds { get; set; }
     public string Name { get; set; }
-    public string CurrentSongId { get; set; }
+    public int CurrentSongPosition { get; set; }
+
+    public string CurrentSongId {
+      get { return SongIds.ElementAt(CurrentSongPosition); }
+    }
 
     public PlayList() {
       SongIds = new List<string>();
+    }
+
+    public void MoveToNextSong() {
+      CurrentSongPosition = (CurrentSongPosition+1) % SongIds.Count();
     }
   }
 }
--- Fantastica.Api/UserRepository.cs
+++ Fantastica.Api/UserRepository.cs
@@ -1,10 +1,28 @@
-﻿using Fantastica.Api.Entities;
+﻿using System.Linq;
+using Fantastica.Api.Entities;
 using Raven.Client;
 
 namespace Fantastica.Api {
   public class UserRepository : Repository<User> {
     public UserRepository(IDocumentStore dbStore) {
       _dbStore = dbStore;
+    }
+
+    public string NextSongId(string userName, string playList) {
+      using (var session = _dbStore.OpenSession()) {
+        var user = session.Query<User>().FirstOrDefault(u => u.Name == userName);
+        if (user == null)
+          return "";
+
+        var userList = user.Lists.FirstOrDefault(e => e.Name == playList);
+        if (userList == null)
+          return "";
+
+        userList.MoveToNextSong();
+
+        session.SaveChanges();
+        return userList.CurrentSongId;
+      }
     }
   }
 }
